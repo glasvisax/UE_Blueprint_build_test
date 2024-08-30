0c144a763b5badf45a6fbd9cc5c5f610b047aebd
WIN-PV0382U21GT\User <1icyscarecrow@gmail.com>
2024-08-30 18:51:24 +0300

    first_commit

---
+++ build.bat
@@ -0,0 +1,6 @@
+"D:\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat" ^
+BuildCookRun ^
+-project="D:\UE_5.3\Test\MyProject2\MyProject2.uproject" ^
+-platform=Win64 -clientconfig=Development ^
+-archivedirectory=D:\UE_5.3\Test\MyProject2\BuildFromBatch ^
+-cook -stage -archive -pak -package -allmaps
